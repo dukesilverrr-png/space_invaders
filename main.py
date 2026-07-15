6940504590fe600b4582e11e9e6d10323cd77455
Duke Silver <duke.silverrr@gmail.com>
2026-07-15 19:50:32 -0400

    Draw basic canvas

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from graphics import Canvas
+
+CANVAS_WIDTH = 500
+CANVAS_HEIGHT = 500
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    canvas.create_rectangle(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT, "black")
+    input("Press Enter to close the window...")
+if __name__ == "__main__":
+    main()
